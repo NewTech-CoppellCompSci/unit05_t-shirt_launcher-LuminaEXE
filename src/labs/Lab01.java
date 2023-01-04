917d541a3c2a1c87ab1a4df5f39e1440f3103da8
LuminaEXE <86614978+LuminaEXE@users.noreply.github.com>
2023-01-03 18:06:44 -0600

    sd
    sd

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -18,31 +18,62 @@ public class Lab01 {
 	
 	
 	public static void problem1() {
-	
+		Scanner sc = new Scanner(System.in);
+		int num1 = intIn(sc,"Enter #1");
+		int num2 = intIn(sc,"Enter #2");
+		
+		System.out.println(num1+"^"+num2 + " = " + Math.pow(num1, num2));
 		
 	}
 	
 	
 	
 	public static void problem2() {
+		System.out.println("\n");
 		
+		Scanner sc = new Scanner(System.in);
+		int number = intIn(sc, "Enter a positive integer");
+		System.out.println("The square root of "+number+" is "+Math.sqrt(number));
 			
 	}
 	
 	
 	
 	public static void problem3() {
+		System.out.println("\n");
 		
+		Scanner sc = new Scanner(System.in);
+		int a = intIn(sc,"Enter a positive integer");
+		int b = intIn(sc,"Enter a positive integer");
 		
+		System.out.println(
+			"Hypotenuse = "+
+			Math.sqrt(
+					Math.pow(a,2)+Math.pow(b,2)
+			)
+		);
 	}
-	
 	
 	
 	public static void problem4() {
+		System.out.println("\n");
 		
+		Scanner sc = new Scanner(System.in);
 		
+		boolean flag = false;
+		do {
+			int num = intIn(sc,"Enter a number: ");
+			
+			if(num == 0) {
+				flag = true;
+			}
+		}while(flag);
 	}
 	
-	
+	public static int intIn(Scanner sc, String user) {
+		System.out.print(user+": ");
+		return sc.nextInt();
+	}
+
 	
 }
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -13,16 +13,29 @@ public class Lab02 {
 
 	}
 
-
-	public static void problem1() {
+	public static void problem1() {		
+		Scanner sc = new Scanner(System.in);
+		int max = intIn(sc,"Enter the max");
+		int min = intIn(sc,"Enter the min");
 		
-		
+		for(int i = 0; i < 100; i++) {
+			int random = randomNumber(min, max);	
+			System.out.print(random+" ");
+		}
 	}
 
 
 	public static void problem2() {
+		System.out.println("\n");
 		
+		Scanner sc = new Scanner(System.in);
 		
+		int radius = intIn(sc,"Enter the radius");
+		int height = intIn(sc,"Enter the height");
+		double pie=3.14285714286;  
+		
+		double volume = pie*Math.pow(radius, 2)*height; 
+		System.out.println("Volume of the cylinder = "+volume);  
 	}
 
 
@@ -38,6 +51,13 @@ public class Lab02 {
 		
 	}
 
-
-
+	public static int randomNumber(int min, int max) { 
+		
+		int b = (int)(Math.random()*(max-min+1)+min);  
+		return b;  
+	}
+	public static int intIn(Scanner sc, String user) {
+		System.out.print(user+": ");
+		return sc.nextInt();
+	}
 }
